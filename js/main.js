1f56aa5c7a7349340911d2517e4982e634c39e48
Pikarestiv <okwyodumodu@gmail.com>
2020-11-07 23:34:30 +0100

    Got current temp from OWM

--- js/main.js
+++ js/main.js
@@ -1,15 +1,38 @@
-var x = document.getElementById("demo");
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+// axios = require("axios");
+apiKey = "b03296b4cd13cee124eefb895fcebf4f";
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  let lat = crd.latitude;
+  let lon = crd.longitude;
+
+
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=metric`;
+
+  axios.get(url)
+    .then(
+      (res) => {
+        var currTemp = res.data.current.temp;
+        document.querySelector("#currTemp").innerHTML = currTemp;
+        console.log(res.data);
+      }
+    )
+    .catch(
+      (err) =>{
+        console.log(err);
+      }
+    )
 }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
-getLocation();
+navigator.geolocation.getCurrentPosition(success, error, options);
